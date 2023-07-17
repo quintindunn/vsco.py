4cbd6eaffe3134db6d3db5eac731ae69951ab5aa
Quintin Dunn <93884113+quintindunn@users.noreply.github.com>
2023-07-17 19:40:20 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 from setuptools import setup
 
+VERSION = "0.0.1"
+DESCRIPTION = "A Python API wrapper for VSCO."
+LONG_DESCRIPTION = "A Python (unofficial) API wrapper for VSCO."
+
 setup(name='vsco',
-      version="0.0.1",
-      description="""A api wrapper for vsco.""",
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description_content_type="text/markdown",
+      long_description=LONG_DESCRIPTION,
       author="Quintin Dunn",
       author_email="dunnquintin07@gmail.com",
-      url="",
+      url="https://github.com/quintindunn/vsco.py",
       packages=['vsco'],
+      keywords=['social media', 'vsco', 'api', 'api wrapper'],
       classifiers=[
             'Development Status :: 5 - Production/Stable',
             'Operating System :: Microsoft :: Windows :: Windows 10',
             'Programming Language :: Python :: 3',
       ],
       install_requires=["pillow", "requests"]
-)
+      )
